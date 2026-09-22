5832634fb4738483c28b2a4f8e9b83cfb729c01c
Netsita7 <orthodoxawit7@gmail.com>
2026-09-22 12:15:11 +0300

    add TeacherStudentDetail export to teacher feature module

--- src/presentation/features/teacher/index.ts
+++ src/presentation/features/teacher/index.ts
@@ -1,3 +1,4 @@
 export { TeacherDashboard } from "./TeacherDashboard";
 export { GradeEntryView } from "./GradeEntryView";
 export { DisciplineView } from "./DisciplineView";
+export { TeacherStudentDetail } from "./TeacherStudentDetail";
