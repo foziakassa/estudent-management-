772f88e7f8ded31b5a40398339cae229f12f4720
Netsita7 <orthodoxawit7@gmail.com>
2026-09-22 12:15:42 +0300

    add TablePagination export to shared components

--- src/presentation/components/shared/index.ts
+++ src/presentation/components/shared/index.ts
@@ -1,4 +1,5 @@
 export { StatusBadge } from "./StatusBadge";
+export { TablePagination } from "./TablePagination";
 export { StatCard } from "./StatCard";
 export { SubjectPerformanceList } from "./SubjectPerformanceList";
 export { StarRating } from "./StarRating";
